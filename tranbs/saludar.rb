a8d0b7dbf8595d328be3aa4ba825960ca619497b
saul <saul_rangel.r1294@hotmail.com>
2016-05-24 01:04:53 -0500

    clear

--- tranbs/saludar.rb
+++
@@ -1,9 +0,0 @@
-def saludar(nombre)
-               puts "Hola #{nombre}".center(80, ' ')
-               puts "\n\n\n\t Teclea para continuar"
-               gets.chomp 
-               system('clear')
-end
-puts "Teclea tu nombre: "
-nombre = gets.chomp
-saludar(nombre)
